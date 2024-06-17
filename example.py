798621566e9e6e127d1bc3ad6d232fa2f22d2747
Ahmad Azaan <104677140+ahmadazaankhan@users.noreply.github.com>
2024-06-18 01:14:40 +0500

    Update example.py

--- example.py
+++ example.py
@@ -1,3 +1,10 @@
 print("A 'single quote' inside a double quote")
 print('A "double quote" inside a single quote')
 print("Alternatively you can just \"escape\" the quote")
+
+print("Hello world \nHello World")
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
