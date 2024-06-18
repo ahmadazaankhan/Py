6e393d82d66735611e4ee0fa4d9f474519f65794
Ahmad Azaan <104677140+ahmadazaankhan@users.noreply.github.com>
2024-06-18 20:35:50 +0500

    Update len.py

--- len.py
+++ len.py
@@ -1,3 +1,32 @@
 numOfLetters = len("Angela")
 print(numOfLetters)
+
 print(len(input()))
+
+name = input("What is your name?")
+print(name)
+
+name = "Jack"
+print(name)
+
+name = "Angela"
+print(name)
+
+
+name = input("What is your name?")
+length = len(name)
+print(length)
+
+name = input("What is your name")
+length = len(name)
+print(length)
+
+a = input()
+b = input()
+
+c = a
+a = b
+b = c
+
+print("a: " + a)
+print("a: " + b)
