97cc82cf998808056a346d8174d08e22ad6f91ce
Ahmad Azaan <104677140+ahmadazaankhan@users.noreply.github.com>
2024-06-19 16:55:15 +0500

    Update datatypes.py

--- datatypes.py
+++ datatypes.py
@@ -8,3 +8,18 @@ print("Hello"[4])
 
 # Subscript
 [0][1][2]
+
+# INTEGAR
+# Integar is a programming lingo for just whole numbers, numbers without any decimal places. In order to declare integar just write numbers
+
+print(123 + 345)
+
+123_456_789
+
+# FLOAT
+# When the number has the decimal places they are called a float and this is short for floating-point number
+
+3.14159 
+
+# BOOLEAN
+# has two types, true of false.
