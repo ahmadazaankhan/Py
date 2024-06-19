865467d6fa75d2f55607810a23ebae65da3cda29
Ahmad Azaan <104677140+ahmadazaankhan@users.noreply.github.com>
2024-06-19 16:23:51 +0500

    DT

---
+++ datatypes.py
@@ -0,0 +1,10 @@
+# DATA TYPES
+
+# STRING
+# In Python, strings are used for representing textual data. A string is a sequence of characters enclosed in either single quotes ('') or double quotes (“”).
+
+print("Hello"[1])
+print("Hello"[4])
+
+# Subscript
+[0][1][2]
