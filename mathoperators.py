61259ed65f6c3fb3cb22d606384fcd0de45b517b
Ahmad Azaan <104677140+ahmadazaankhan@users.noreply.github.com>
2024-06-22 17:22:46 +0500

    calc

--- mathoperators.py
+++ mathoperators.py
@@ -22,12 +22,34 @@
 # bmi_int = int(bmi)
 # print(bmi_int)
 
-height = input()
-weight = input()
+# height = input()
+# weight = input()
 
-weight_as_int = int(weight)
-height_as_float = float(height)
+# weight_as_int = int(weight)
+# height_as_float = float(height)
 
-bmi = weight_as_int / (height_as_float * height_as_float)
-bmi_as_int = int(bmi)
-print(bmi_as_int)
+# bmi = weight_as_int / (height_as_float * height_as_float)
+# bmi_as_int = int(bmi)
+# print(bmi_as_int)
+
+# print(type(8//3))
+
+# result = 4 / 2
+# result /= 2
+# print(result)
+
+# score = 0
+# score +=1
+# print(score)
+
+# score = 4
+# height = 1.8
+# isWinning = True
+# #F-String
+# print(f"your score is {score}, your height is {height}, you are winning is {isWinning}")
+
+age = input()
+years = 90 * int(age)
+weeks = years * 52
+
+print(f"You have {weeks} weeks left")
---
+++ tip_calc.py
@@ -0,0 +1,13 @@
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? $"))
+tip = input("How much tip would you like to give? 10, 12, or 15? ")
+people = int(input("How many people to split the bill? "))
+# bill_w_tip = bill * (1 + int(tip) / 100)
+# bill_w_tip = int(tip) / 100 * bill + bill
+# print(bill_w_tip)
+tip_as_perc = int(tip) / 100
+total_tip = bill * tip_as_perc
+total_bill = bill + total_tip
+bill_per_person = total_bill / people
+final_amount = round(bill_per_person, 2)
+print(f"Each person should pay: ${final_amount}")
