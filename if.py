684ed27b71960bbe2194ed287a88b400b4977549
Ahmad Azaan <104677140+ahmadazaankhan@users.noreply.github.com>
2024-06-23 02:29:21 +0500

    if nested

---
+++ if.py
@@ -0,0 +1,28 @@
+# Comparison operators
+
+# > Greater than
+# < Less than
+# >= Greater than or equal to
+# <= Less than or equal to 
+# == Equal to
+# != Not Equal to
+
+
+
+
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+  print("You can ride the rollercoaster")
+else:
+  print("Sorry, you have to grow taller before you can ride")  
+
+
+
+
+number = int(input())
+if number % 2 == 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
