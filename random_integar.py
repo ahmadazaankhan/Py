12aaea425f3ab9cb52b41c9957b26d9814112a83
Ahmad Azaan <104677140+ahmadazaankhan@users.noreply.github.com>
2024-06-25 17:36:47 +0500

    dice

---
+++ random_integar.py
@@ -0,0 +1,14 @@
+import random
+import my_module
+
+random_integar = random.randint(1, 10)
+print(random_integar)
+
+random_float = random.random()
+print(random_float)
+
+decimal = random.random() *5
+print(decimal)
+
+love_score = random.randint(1, 100)
+print(f"Your love score is {love_score}")
