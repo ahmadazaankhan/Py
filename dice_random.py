db82b71c2cc7e2bf8c67d48a261cd2a4315bcb81
Ahmad Azaan <104677140+ahmadazaankhan@users.noreply.github.com>
2024-06-25 22:14:08 +0500

    Update dice_random.py

--- dice_random.py
+++ dice_random.py
@@ -6,3 +6,13 @@ if random_side == 1:
   print("Heads")
 else:
   print("Tails")
+
+
+import random
+
+random_side = random.randint(0, 1)
+
+if random_side == 1:
+  print("Heads")
+else:
+  print("Tails")
