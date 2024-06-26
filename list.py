9b77bbd94ff5c035edcc1e1369b1aa7c368f095a
Ahmad Azaan <104677140+ahmadazaankhan@users.noreply.github.com>
2024-06-27 00:39:17 +0500

    Create list.py

---
+++ list.py
@@ -0,0 +1,29 @@
+# PYTHON LIST
+
+# Concept of python list, the list is what you would call a data structure
+
+# Data structure is just a way of organizing and storing data in python
+
+# List is a set of square brackets with many items stored inside
+# ex:
+# fruits = [item1, item2]
+
+# a = 3
+# b = "hello"
+
+# state1 = "Delware"
+
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois", "Alabama", "Maine", "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin", "California", "Minnesota", "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"]
+
+print(states_of_america[12])
+print(states_of_america[-1])
+
+states_of_america[1] = "Pencil"
+print(states_of_america)
+
+states_of_america.append("Angeland")
+
+
+states_of_pak = ['Punjab', 'Balochistan', 'KPK', 'Sindh']
+
+print(states_of_pak[2])
