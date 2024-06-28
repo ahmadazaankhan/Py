f4a2c5a710d82fd3735bfd163ff570f29c7970b1
Ahmad Azaan <104677140+ahmadazaankhan@users.noreply.github.com>
2024-06-28 20:05:11 +0500

    even

--- loops.py
+++ loops.py
@@ -6,3 +6,21 @@
 fruits = ["Apple", "Peach", "Pear"]
 for fruit in fruits:
   print(fruit)
+  print(fruit + " Pie")
+# Indentation is imp in python
+print(fruits)
+
+
+# range fucntion
+
+# for number in range(1, 11):
+#   print(number)
+
+# for number in range(1, 11, 3):
+#   print(number)
+
+
+total = 0
+for number in range (1, 101):
+  total += number
+print(total)
