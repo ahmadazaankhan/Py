ea869d87913c71cdd4a14282a5f4845141c54397
Ahmad Azaan <104677140+ahmadazaankhan@users.noreply.github.com>
2024-06-30 21:56:57 +0500

    ex

---
+++ input_func.py
@@ -0,0 +1,32 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+# def greet():
+#   print("Hello Ahmad")
+#   print("How's it going Ahmad")
+#   print("Isn't the weather nice today?")
+
+# greet()
+
+#Function that allows for input
+
+# def greet_with_name(name):
+#   print(f"Hello {name}")
+#   print(f"How's it going {name}")
+#   print("Isn't the weather nice today?")
+
+# greet_with_name("Ahmad")
+
+#Functions with more than 1 input
+def greet_with(name, location):
+  print(f"Hello {name}")
+  print(f"What is it like in {location}?")
+  # print(f"What date is it {date}?")
+
+# greet_with("Ahmad", "Lahore", "01/01/2001")
+
+#Functions with keyboard arguements
+greet_with(name = "Angela", location = "London" )
+
