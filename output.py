cb0d05d18e272ebb39b17b0ba471d4b8412b9f1e
Ahmad Azaan <104677140+ahmadazaankhan@users.noreply.github.com>
2024-07-02 17:37:31 +0500

    Update output.py

--- output.py
+++ output.py
@@ -11,10 +11,17 @@
 
 # format_name("angela", "ANGELA")
 
-def format_name(f_name, l_name):
-    formated_f_name = f_name.title()
-    formated_l_name = l_name.title()
-    return f"{formated_f_name} {formated_l_name}"
+# def format_name(f_name, l_name):
+#     formated_f_name = f_name.title()
+#     formated_l_name = l_name.title()
+#     return f"{formated_f_name} {formated_l_name}"
 
-formated_string = format_name("AnGELAA", "ATY")
-print(formated_string)
+# # formated_string = format_name("AnGELAA", "ATY")
+# print(format_name("ANGELa", "YuU"))
+
+
+def name_format(g_name, r_name):
+    formated_g_name = g_name.title()
+    formated_r_name = r_name.title()
+    return f"{formated_g_name} {formated_r_name}"
+print(name_format("AsHeeR", "haDDDI"))
