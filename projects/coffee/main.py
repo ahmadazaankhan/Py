a05602b813860b5fe13b5fb7343e1d3200a312be
Ahmad Azaan <104677140+ahmadazaankhan@users.noreply.github.com>
2024-07-13 17:56:59 +0500

    cf

--- projects/coffee/main.py
+++ projects/coffee/main.py
@@ -24,9 +24,45 @@ MENU = {
     }
 }
 
+profit = 0
 resources = {
     "water": 300,
     "milk": 200,
     "coffee": 100,
 }
 
+
+def is_resource_sufficient(order_ingredients):
+    """Returns True when order can be made, False if ingredients are insufficient."""
+    for item in order_ingredients:
+        if order_ingredients[item] >= resources[item]:
+            print(f"Sorry there is not enough {item}.")
+            return False
+        return True
+
+
+def process_coins():
+    """"Returns the total calculated from coins inserted"""
+    print("Please insert coins.")
+    total = int(input("How many quarters?: ")) * 0.25
+    total = int(input("How many dimes?: ")) * 0.1
+    total = int(input("How many nickles?: ")) * 0.05
+    total = int(input("How many pennies?: ")) * 0.01
+    return total
+
+
+is_on = True
+
+while is_on:
+    choice = input("What would you like? (Espresso/Latte/Cappuccino): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        print(f"Water: {resources['water']}ml")
+        print(f"Milk: {resources['milk']}ml")
+        print(f"Coffee: {resources['coffee']}g")
+        print(f"Money: ${profit}")
+    else:
+        drink = MENU[choice]
+        if is_resource_sufficient(drink["ingredients"]):
+            payment = process_coins()
