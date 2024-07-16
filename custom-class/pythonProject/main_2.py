554f2a3ebd5c33004f4e7de579b5accc2d412361
Ahmad Azaan <104677140+ahmadazaankhan@users.noreply.github.com>
2024-07-16 15:29:41 +0500

    object OOP
    classes OOP method // quiz

--- custom-class/pythonProject/main_2.py
+++ custom-class/pythonProject/main_2.py
@@ -1,14 +1,36 @@
 class User:
 
-    def __init__(self, user_id, username):
+    def __init__(self, user_id, username, follower):
         self.id = user_id
         self.username = username
+        self.followers = follower
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
 
 
-user_1 = User("001", "asher")
+user_1 = User("001", "asher", "1M")
+user_2 = User("002", "shahzain", "10M")
 
 print(user_1.id)
+print(user_2.followers)
+
 
 # user_2 = User()
 # user_2.id = "002"
 # user_2.name = "jack"
+
+# class Person:
+#
+#     def __init__(self, user_id, username):
+#         self.id = user_id
+#         self.username = username
+#
+#
+# person_1 = Person("003", "hadi")
+# person_2 = Person("004", "Zoya")
+#
+# print(person_1.username)
+# print(person_2.id)
--- day-16-start/day-16/another_module.py
+++ day-16-start/day-16/another_module.py
@@ -1,1 +1,19 @@
-another_variable = 12
+class Employee:
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@company.com'
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+
+emp_1 = Employee('Ahmad', 'Azaan', '50000')
+emp_2 = Employee('Joey', 'Tribbiani', '70000')
+
+print(emp_1.email)
+print(emp_2.email)
+print(emp_2.fullname())
+print(emp_2.fullname())
